69b4e91118cff170afc3c14b824cf7b074af10f2
somrat <jafaruddin875@gmail.com>
2022-09-07 23:06:48 +0200

    divided the header part in small section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-         <h1>Eshop</h1>
+         <Header/>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,26 @@
+import "./Header.css"
 export default function Header(){
     return (
-      <div>
+      <div className="header">
+           <div className="header__logo">
 
-        
+           </div>
+           <div className="header__search">
+
+
+           </div>
+           <div className="header__nav">
+            <div className="nav__item">
+
+            </div>
+            <div className="nav__item">
+                
+            </div>
+            <div className="nav__item">
+                
+            </div>
+           </div>
+
       </div>
 
 
