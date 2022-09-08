b806fd9c19c47aed6ad0f3209fe3a1b08e8228a0
somrat <jafaruddin875@gmail.com>
2022-09-08 23:14:23 +0200

    set Image on Home page

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,15 @@ import "./Home.css"
 
 export default function Home(){
       return(
-        <h1>Home</h1>
+        <div>
+             <div className="home">
+                <div className="home__container">
+                     <img src="https://www.x-cart.com/wp-content/uploads/2019/01/ecommerce-768x278.jpg" alt="e-shop Image" className="home__image"></img>
+                 </div>
+             </div>
+
+
+        </div>
       )
 
 }
