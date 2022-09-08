e0439395b76c85b72f44f6934c2506378eb41d81
somrat <jafaruddin875@gmail.com>
2022-09-08 23:16:44 +0200

    Creating row for Showing Product

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,19 @@ export default function Home(){
              <div className="home">
                 <div className="home__container">
                      <img src="https://www.x-cart.com/wp-content/uploads/2019/01/ecommerce-768x278.jpg" alt="e-shop Image" className="home__image"></img>
+
+                     <div className="home_row">
+
+
+                     </div>
+                     <div className="home_row">
+
+                        
+                     </div>
+                     <div className="home_row">
+
+                        
+                     </div>
                  </div>
              </div>
 
