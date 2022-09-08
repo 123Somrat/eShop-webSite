5bab7aa0997ce02fc4a7e315f8dc6a350482ffe8
somrat <jafaruddin875@gmail.com>
2022-09-08 23:43:50 +0200

    create product

--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,16 @@
+import "./Product.css"
 export default function Product(){
     return(
         <div>
-            <h2>Product</h2> 
-            
+            <div className="product">
+                <div className="product__info">
+                    <p>Title</p>
+                    <p className="product__price">$30</p>
+                     <div className="product__rating">⭐⭐⭐⭐</div>
+                </div>
+                <img src="https://m.media-amazon.com/images/I/91I7CmeK6lL._AC_SX301_SY283_.jpg" alt="black_laptop_shoulder Bag"></img>
+                <button>Add To Basket</button>
+            </div>
         </div>
     )
 
