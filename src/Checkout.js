66eae7962fcc0160fbd64cbab8940f5fa52a2a1f
somrat <jafaruddin875@gmail.com>
2022-09-15 12:08:04 +0200

    add advertisement

--- src/Checkout.js
+++ src/Checkout.js
@@ -4,11 +4,13 @@ export default function Checkout() {
     return (
       <div className="checkout">
         <div className="checkout__left">
+           <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/
+               OCC_Amazon1._CB423492668_.jpg"  alt="This is a add pic"className="checkout__ad" />
 
         </div>
          <div className="checkout__right">
 
-          
+
          </div>
       </div>
 
