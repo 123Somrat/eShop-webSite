59c5c8f9cec33eac0be61a69fcfe0d10c17ba86c
somrat <jafaruddin875@gmail.com>
2022-09-15 13:33:05 +0200

    create a format useing rendertext

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -6,7 +6,22 @@ export default function Subtotal() {
   return (
     <div className='subtotal'>
        <CurrencyFormat
-       
+            renderText={(value)=>(
+               <>
+                  <p>
+                      Subtotal (0 items):<strong>$0</strong>
+                  </p>
+                  <small className='subtotal__gift'>
+                    <input type="checkbox" />This order contains a gift
+
+                  </small>
+               </>
+            )}
+            decimalScale={2}
+            value={0}
+            displayType={"text"}
+            thousandSeparator={true}
+            
        />
 
     </div>
