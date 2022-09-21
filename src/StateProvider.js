cf9e1a5d730c0cb76836e77366f11528c899fff0
somrat <jafaruddin875@gmail.com>
2022-09-21 11:55:02 +0200

    creating provider

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -2,3 +2,11 @@ import {createContext,useContext,useReducer} from "react"
 
 // prepares the data layer
 export const StateContext = createContext()
+
+// wrap our app and provide the data layer
+
+export const StateProvider = ({reducer,initialState,children}) => {
+    <StateContext.Provider value={useReducer(reducer,initialState)}>
+        {children}
+    </StateContext.Provider>
+}
