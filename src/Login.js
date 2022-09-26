b171e32d8c82527662882204f96cec0aa2262d86
somrat <jafaruddin875@gmail.com>
2022-09-26 14:58:49 +0200

    add signIn button in form

--- src/Login.js
+++ src/Login.js
@@ -8,7 +8,7 @@ function Login(){
     const [password,setPassword] = useState("")
    return(
        <div className='login'>
-          <Link>
+          <Link to="./login" style={{textDecoration:"none"}}>
              <div className='login__logo'>
                    <StorefrontIcon className='login__logoImage' fontSize='large' />
                    <h2 className='login__logoTitle'>eShop</h2>
@@ -21,6 +21,7 @@ function Login(){
                      <input type="text" value={email}onChange={e=>setEmail(e.target.value)}/>
                      <h5>Password</h5>
                      <input type="password" value={password} onChange={e=>setPassword(e.target.value)}/>
+                     <button type="submit" className='login_signInButton'>Sign <In></In></button>
                 </form>
                  <p>
                     By signing-in you agree to the eShop Website Conditions of Use & Sale. Please
