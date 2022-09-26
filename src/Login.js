c284a4802dc45d3ef3dfe5bee2a220b90e2bec44
somrat <jafaruddin875@gmail.com>
2022-09-26 14:36:04 +0200

    Link up the page

--- src/Login.js
+++ src/Login.js
@@ -1,15 +1,20 @@
 import StorefrontIcon from '@mui/icons-material/Storefront';
 import "./Login.css"
+import {Link} from "react-router-dom"
 
 function Login(){
    return(
        <div className='login'>
-           <div className='login__logo'>
-                <StorefrontIcon className='login__logoImage' fontSize='large' />
-                <h2 className='login__logoTitle'>eShop</h2>
-           </div>
-       </div>
-
+          <Link>
+             <div className='login__logo'>
+                   <StorefrontIcon className='login__logoImage' fontSize='large' />
+                   <h2 className='login__logoTitle'>eShop</h2>
+                </div>
+       
+            </Link>
+           
+        </div>
+        
    )
 
 }
