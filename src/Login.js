bd1ce835e057e8f5437eb3169491b28b39028d3c
somrat <jafaruddin875@gmail.com>
2022-09-26 14:05:59 +0200

    import storeIcon from meterial ui

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,17 @@
+import StorefrontIcon from '@mui/icons-material/Storefront';
 import "./Login.css"
 
 function Login(){
    return(
-       <div>
-          Login
+       <div className='login'>
+           <div className='login__logo'>
+                <StorefrontIcon className='login__logoImage' fontSize='large' />
+                <h2 className='login__logoTitle'>eShop</h2>
+           </div>
        </div>
 
    )
 
 }
+
+export default Login;
